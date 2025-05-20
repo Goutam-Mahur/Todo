b0a472051b81c85bba7d62260cf776060cfeede4
Goutam Mahur <goutammahur08@gmail.com>
2025-05-20 14:27:47 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
