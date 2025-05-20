5c32e48c639c7fa429418504a54e94f3acb0f815
Goutam Mahur <goutammahur08@gmail.com>
2025-05-20 13:52:11 +0530

    deploy to vercel

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const connectDB = require("./database/db");
 const taskRoutes = require("./routes/task.routes");
 const authRoutes = require("./routes/auth.routes");
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*", credentials: true }));
 app.use(bodyParser.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+if (process.env.VERCEL !== "1") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
